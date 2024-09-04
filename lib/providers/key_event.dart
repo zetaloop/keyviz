bb60e898324adbd89ee38299e74f511b57cd7f5c
zetaloop <zetaloop@outlook.com>
2024-09-04 22:20:20 +0800

    常规页

--- lib/providers/key_event.dart
+++ lib/providers/key_event.dart
@@ -54,8 +54,12 @@ enum VisualizationHistoryMode {
   @override
   String toString() {
     return this == VisualizationHistoryMode.none
-        ? "None"
-        : "${name.capitalize()}ly";
+        ? "不显示"
+        : {
+              'vertical': '垂直显示',
+              'horizontal': '水平显示',
+            }[name] ??
+            "${name.capitalize()}ly";
   }
 }
 
